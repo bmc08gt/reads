9480cf4294c6242b748bbd9958c3849b20acdd20
Brandon McAnsh <brandon.mcansh@gmail.com>
2019-06-05 17:18:37 -0400

    Update color palette and convert camera to FAB
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- app/src/main/java/com/bmcreations/bookinfo/MainActivity.kt
+++ app/src/main/java/com/bmcreations/bookinfo/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.bmcreations.bookinfo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.bmcreations.bookinfo.ui.camera.CameraViewfinder
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +20,12 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
 
         navView.setupWithNavController(navController)
+
+        look_up_book.setOnClickListener {
+            Intent(this@MainActivity, CameraViewfinder::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
